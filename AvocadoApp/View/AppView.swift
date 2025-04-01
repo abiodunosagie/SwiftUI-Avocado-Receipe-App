45414bef892b38d868bc2a314448d7ebf073f9d4
Abiodun Osagie <abiodunosagie8@gmail>
2025-04-01 16:38:12 +0100

    Adding the tab view

---
+++ AvocadoApp/View/AppView.swift
@@ -0,0 +1,18 @@
+//
+//  AppView.swift
+//  AvocadoApp
+//
+//  Created by Abiodun Osagie on 01/04/2025.
+//
+
+import SwiftUI
+
+struct AppView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppView()
+}
---
+++ AvocadoApp/View/AvocadosView.swift
@@ -0,0 +1,18 @@
+//
+//  AvocadosView.swift
+//  AvocadoApp
+//
+//  Created by Abiodun Osagie on 01/04/2025.
+//
+
+import SwiftUI
+
+struct AvocadosView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AvocadosView()
+}
