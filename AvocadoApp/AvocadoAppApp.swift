5d42317594a7745b13c3bcd5b14bee18f34a952b
Abiodun Osagie <abiodunosagie8@gmail>
2025-04-01 15:15:11 +0100

    Initial Commit

---
+++ AvocadoApp/AvocadoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadoAppApp.swift
+//  AvocadoApp
+//
+//  Created by Abiodun Osagie on 01/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct AvocadoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AvocadoApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AvocadoApp
+//
+//  Created by Abiodun Osagie on 01/04/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
